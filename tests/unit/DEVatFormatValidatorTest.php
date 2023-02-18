361e3c07493c10fc7e9471e33b44e53e3f150f1d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-02-18 17:26:54 +0400

    Completed test.

--- tests/unit/DEVatFormatValidatorTest.php
+++ tests/unit/DEVatFormatValidatorTest.php
@@ -31,8 +31,72 @@ class DEVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => '000000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '111111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '999999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DE000000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DE111111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DE999999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DE123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '12A4567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '12A45678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1234A6789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'AT123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'de123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'd123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'e123456789',
+                'isValid' => false,
             ],
         ];
     }
